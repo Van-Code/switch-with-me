b6bca47d62cc06a652834b479c6204679df01bb6
van-code <19809586+Van-Code@users.noreply.github.com>
2025-12-26 17:03:26 -0800

    add aws image path

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   images: {
     domains: [],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "amzn-s3-swm-public-uploads-bucket.s3.us-west-2.amazonaws.com",
+        pathname: "/**",
+      },
+    ],
   },
 }
 
